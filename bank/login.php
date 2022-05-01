800b80e6a22dea86f4d0a6201623535a2a59c290
SAHIL7400 <68220762+SAHIL7400@users.noreply.github.com>
2022-05-01 19:43:58 +0530

    initial commit

---
+++ bank/login.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to SEVA bank. Committed to serve you and your beloved ones.</title>
+	<?php require 'assets/autoloader.php'; ?>
+	<?php require 'assets/function.php'; ?>
+	<?php
+    $con = new mysqli('localhost','root','','Sevabank');
+    define('bankName','',true);
+
+		$error = "";
+		if (isset($_POST['userLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+
+			$stmt = $con->prepare("select * from userAccounts where email=? AND password=?");
+			$stmt->bind_param('ss',$user,$pass);
+			$stmt->execute();
+    		$result = $stmt->get_result();
+		    if($result->num_rows == 1)
+		    {
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['userId'] = $data['id'];
+		      $_SESSION['user'] = $data;
+		      header('location:index.php');
+		     }
+		    else
+		    {
+			  $error = "<div class='alert alert-warning text-center rounded-0'>
+			  <button type='button' class='close' data-dismiss='alert'>&times;</button>
+			  Username or password wrong try again!</div>";
+		    }
+		}
+		if (isset($_POST['cashierLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+
+			$stmt = $con->prepare("select * from login where email=? AND password=?");
+			$stmt->bind_param('ss',$user,$pass);
+			$stmt->execute();
+    		$result = $stmt->get_result();
+		    if($result->num_rows == 1)
+		    {
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['cashId']=$data['id'];
+		      //$_SESSION['user'] = $data;
+		      header('location:cindex.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'><button type='button' class='close' data-dismiss='alert'>&times;</button>Username or password wrong try again!</div>";
+		    }
+		}
+		if (isset($_POST['managerLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+
+			$stmt = $con->prepare("select * from login where email=? AND password=? AND type='manager'");
+			$stmt->bind_param('ss',$user,$pass);
+			$stmt->execute();
+    		$result = $stmt->get_result();
+		    if($result->num_rows == 1)
+		    {
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['managerId']=$data['id'];
+		      //$_SESSION['user'] = $data;
+		      header('location:mindex.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'><button type='button' class='close' data-dismiss='alert'>&times;</button>Username or password wrong try again!</div>";
+		    }
+		}
+
+	 ?>
+</head>
+<body style="background: url(images/bg-login2.jpg);background-size: 100%">
+<h1 class="alert alert-success rounded-0"><?php echo bankname; ?><small class="float-right text-muted" style="font-size: 12pt;"><kbd>Presented by: Sahil Khandelwal(1AY18CS102)</kbd></small><img src="images/logo.png" width="54" height="60" class="d-inline-block align-top" alt=""> SEVA Bank Limited</h1>
+<br>
+<?php echo $error ?>
+<br>
+<div id="accordion" role="tablist" class="w-25 float-right shadowBlack" style="margin-right: 222px">
+	<br><h4 class="text-center text-white">Select Your Session</h4>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingOne">
+      <h5 class="mb-0">
+        <a style="text-decoration: none;" data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         <button class="btn btn-outline-success btn-block">User Login</button>
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+       <form method="POST">
+       	<input type="email"  name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" class="form-control" required placeholder="Enter Password">
+       	<button type="submit" class="btn btn-primary btn-block btn-sm my-1" name="userLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a class="collapsed btn btn-outline-success btn-block" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Manager Login
+        </a>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+         <form method="POST">
+       	<input type="email"  name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password"  class="form-control" required placeholder="Enter Password">
+       	<button type="submit" class="btn btn-primary btn-block btn-sm my-1" name="managerLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a class="collapsed btn btn-outline-success btn-block" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+         Cashier Login
+        </a>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        <form method="POST">
+       	<input type="email" name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" class="form-control" required placeholder="Enter Password">
+       	<button type="submit"  class="btn btn-primary btn-block btn-sm my-1" name="cashierLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
+ All accounts
+  </div>
+  <div class="card-body">
+   <table class="table table-bordered table-sm">
+  <thead>
+    <tr bgcolor='Green'>
+      <th scope="col">#</th>
+      <th scope="col">Holder Name</th>
+      <th scope="col">Account No.</th>
+      <th scope="col">Branch Name</th>
+      <th scope="col">Current Balance</th>
+      <th scope="col">Account type</th>
+      <th scope="col">Contact</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+      $i=0;
+      $array = $con->query("select * from useraccounts,branch where useraccounts.branch = branch.branchId");
+      if ($array->num_rows > 0)
+      {
+        while ($row = $array->fetch_assoc())
+        {$i++;
+    ?>
+      <tr bgcolor='white'>
+        <th scope="row"><?php echo $i ?></th>
+        <td> <?php echo $row['name'] ?></td>
+        <td><?php echo $row['accountNo'] ?></td>
+        <td><?php echo $row['branchName'] ?></td>
+        <td>₹<?php echo $row['balance'] ?></td>
+        <td><?php echo $row['accountType'] ?></td>
+        <td><?php echo $row['phone'] ?></td>
+		<td>
+          <a href="show.php?id=<?php echo $row['id'] ?>" class='btn btn-success btn-sm' data-toggle='tooltip' title="View More info">View</a>
+          <a href="transfer.php" class="btn btn-outline-success btn-block">Transfer Money</a>
+		  </td>
+        
+        
+      </tr>
+    <?php
+        }
+      }
+     ?>
+  </tbody>
+</table>
+
+  
